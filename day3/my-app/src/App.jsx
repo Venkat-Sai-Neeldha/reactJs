0fcdd75927c49fe8f8abe60320d49174d4a5743a
Venkat-Sai-Neeldha <144706211+Venkat-Sai-Neeldha@users.noreply.github.com>
2025-03-06 11:44:35 +0530

    updated changes

--- day3/my-app/src/App.jsx
+++ day3/my-app/src/App.jsx
@@ -1,11 +1,13 @@
 
 import { useState } from 'react'
 import './App.css'
-
+import Memo from './Memo'
+import Callback from './Callback'
+import Ref from './Ref'
 function App() {
   return(<>
-   < Memo/>
-   <Callback/>
+     < Memo/>
+    <Callback/>
      <Ref/>
   </>)
 }
