5936426d8a30f6516460fa06850456541dfc0c6c
Venkat-Sai-Neeldha <144706211+Venkat-Sai-Neeldha@users.noreply.github.com>
2025-04-03 18:27:04 +0530

    update

--- uitask/src/components/Image.jsx
+++ uitask/src/components/Image.jsx
@@ -3,15 +3,16 @@ import './Image.css'
 function Image() {
   return (
    <div className="images">
-    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Birthday_Desk-28-03.jpg" alt="aa" />Birthday</div>
-    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/2hourdelivery_Squircle_41224.jpg" alt="aa" />two hour delivery</div>
-    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/luxe_new_desk-09-02-2025.jpg" alt="aa" />FNP luxe</div>
-    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Hatke_gifts_Desk.jpg" alt="aa" />Hatke-gifts</div>
-    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Navratri_Desk-29-03.jpg" alt="aa" />Navrathri</div>
-    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Anniversary_desk_10-03-2025.jpg" alt="aa" />Anniversary</div>
-    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Flowers_Squircle.jpg" alt="aa" />Flowers</div>
-    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Cakes-desk-01-04.jpg" alt="aa" />Cakes</div>
-    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Kids_Corner_Desk_01-04.jpg" alt="aa" />Kids Corner</div>
+    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Birthday_Desk-28-03.jpg" alt="aa" /><span>Birthday</span></div>
+    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/2hourdelivery_Squircle_41224.jpg" alt="aa" /><span>
+    two hour delivery</span></div>
+    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/luxe_new_desk-09-02-2025.jpg" alt="aa" /><span>FNP luxe</span></div>
+    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Hatke_gifts_Desk.jpg" alt="aa" /><span>Hatke-gifts</span></div>
+    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Navratri_Desk-29-03.jpg" alt="aa" /><span>Navrathri</span></div>
+    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Anniversary_desk_10-03-2025.jpg" alt="aa" /><span>Anniversary</span></div>
+    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Flowers_Squircle.jpg" alt="aa" /><span>Flowers</span></div>
+    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Cakes-desk-01-04.jpg" alt="aa" /><span>Cakes</span></div>
+    <div className="img2"><img src="https://www.fnp.com/assets/images/custom/new-desk-home/hero-banners/Kids_Corner_Desk_01-04.jpg" alt="aa" /><span>Kids Corner</span></div>
 
    
    </div>
